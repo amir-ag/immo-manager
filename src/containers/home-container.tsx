0c93083c78be56dd99233c4bbf98df00e05b33bc
Amir <40580705+amir-ag@users.noreply.github.com>
2021-09-08 19:28:18 +0200

    add routing and refactor folder structure prepare redux

---
+++ src/containers/home-container.tsx
@@ -0,0 +1,11 @@
+// import { connect } from "react-redux";
+import Home from "../components/Home/Home";
+// import functions from selectors
+
+// function mapStateToProps(): {isLoggedIn: boolean} {
+//     return {
+//         isLoggedIn: false
+//     };
+// }
+
+export const HomeContainer = Home;
