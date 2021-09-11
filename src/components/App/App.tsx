b04e5f4298d369abf995350902446bde0655d373
Amir <40580705+amir-ag@users.noreply.github.com>
2021-09-11 21:56:38 +0200

    add routes constant file

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import {HomeRoute} from "../../routes/home-route";
 import './App.css';
+import routes from "../../routes/constant";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/" component={HomeRoute}/>
+          <Route exact path={routes.HOME} component={HomeRoute}/>
         </Switch>
       </Router>
     </div>
---
+++ src/routes/constant.ts
@@ -0,0 +1,6 @@
+const routes = {
+    HOME: '/',
+    DASHBOARD: '/dashboard'
+}
+
+export default routes;
