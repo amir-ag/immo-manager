fae82e7f652cf54e943a1116a30c4b7a951cb41a
Amir <40580705+amir-ag@users.noreply.github.com>
2021-09-12 22:01:33 +0200

    add list items on drawer

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {LayoutProps} from "./types";
-import {Drawer, makeStyles, Typography} from "@material-ui/core";
+import {Drawer, List, ListItem, ListItemIcon, ListItemText, makeStyles, Typography} from "@material-ui/core";
+import HomeWorkOutlinedIcon from '@material-ui/icons/HomeWorkOutlined';
+import PersonAddOutlinedIcon from '@material-ui/icons/PersonAddOutlined';
+import {useHistory, useLocation} from "react-router";
 
 const drawerWidth = 240;
 
@@ -17,12 +20,30 @@ const useStyles = makeStyles({
     },
     root: {
         display: 'flex',
+    },
+    active: {
+        background: '#f4f4f4',
     }
 })
 
 const Layout = ({children}: LayoutProps) => {
 
     const classes = useStyles();
+    const history = useHistory();
+    const location = useLocation();
+
+    const menuItems = [
+        {
+            text: 'Create Property',
+            icon: <HomeWorkOutlinedIcon color={"secondary"}/>,
+            path: '/dashboard/property'
+        },
+        {
+            text: 'Create Person',
+            icon: <PersonAddOutlinedIcon color={"secondary"}/>,
+            path: '/dashboard/people'
+        },
+    ];
 
     return (
         <div className={classes.root}>
@@ -30,12 +51,27 @@ const Layout = ({children}: LayoutProps) => {
                 className={classes.drawer}
                 variant={"permanent"}
                 anchor={"left"}
-                classes={{ paper: classes.drawPaper }}
+                classes={{paper: classes.drawPaper}}
             >
                 <Typography variant={"h5"}>
                     Immo Manager v1
                 </Typography>
+
+                <List>
+                    {menuItems.map(item => (
+                        <ListItem
+                            key={item.text}
+                            button
+                            onClick={() => history.push(item.path)}
+                            className={location.pathname === item.path ? classes.active : undefined}
+                        >
+                            <ListItemIcon>{item.icon}</ListItemIcon>
+                            <ListItemText primary={item.text}/>
+                        </ListItem>
+                    ))}
+                </List>
             </Drawer>
+
             <div className={classes.page}>
                 {children}
             </div>
