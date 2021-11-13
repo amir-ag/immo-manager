1f0aca178bafc265454c0388657e140db638c7f8
Amir <40580705+amir-ag@users.noreply.github.com>
2021-11-13 09:42:45 +0100

    add profile basic layout

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
+import { Container, makeStyles, Paper, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+    profile: {
+        height: '50vh',
+    },
+    mainContainer: {
+        display: 'flex',
+        height: '95%',
+    },
+    rightContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+    },
+    rightTop: {
+        height: '50%',
+    },
+    rightBottom: {
+        height: '50%',
+    },
+}));
 
 const Profile = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, adipisci architecto,
-            consequuntur debitis ducimus earum error in minima molestias, natus nihil omnis possimus quae?
-            Architecto eum impedit sit voluptate. Vitae!
+        <div className={classes.profile}>
+            <Typography variant={'h4'}>Profile</Typography>
+            <Container className={classes.mainContainer} component={Paper} elevation={0}>
+                <Container>
+                    <Typography variant={'body2'}>General Info</Typography>
+                </Container>
+                <Container className={classes.rightContainer}>
+                    <div className={classes.rightTop}>
+                        <Typography variant={'body2'}>Address</Typography>
+                    </div>
+                    <div className={classes.rightBottom}>
+                        <Typography variant={'body2'}>Password</Typography>
+                    </div>
+                </Container>
+            </Container>
         </div>
     );
 };
