ea0f63db762e02a71a590934b65f5f9dab314fbc
Amir <40580705+amir-ag@users.noreply.github.com>
2021-12-25 12:14:25 +0100

    read property id from params

--- src/components/rent-schedule/rent-schedule-table.tsx
+++ src/components/rent-schedule/rent-schedule-table.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const RentScheduleTable = () => {
-    return <div>This will be a table</div>;
+    const { id } = useParams<{ id: string }>();
+    console.log('params: ', id);
+
+    return <div>This will be a table for property: {id}</div>;
 };
 
 export default RentScheduleTable;
