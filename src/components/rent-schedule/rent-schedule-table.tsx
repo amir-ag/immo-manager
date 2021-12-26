3876dd8e5af05a40d61a04767fa7f081aa8f5e0a
Amir <40580705+amir-ag@users.noreply.github.com>
2021-12-26 18:07:50 +0100

    implement property header

--- src/components/rent-schedule/rent-schedule-table.tsx
+++ src/components/rent-schedule/rent-schedule-table.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import { useAppSelector } from '../../store/hooks';
 import { selectProperties, selectRentalUnits, selectTenancies } from '../../store/selectors';
@@ -17,6 +17,9 @@ import {
 import { getDisplayNameOfRentalUnit, RentalUnitModel } from '../rental-unit/model/rental-unit.model';
 import { emptyTenancy } from '../tenancy/model/tenancy.model';
 import GetAppOutlinedIcon from '@material-ui/icons/GetAppOutlined';
+import { useReactToPrint } from 'react-to-print';
+import { format } from 'date-fns';
+import { getDisplayNameOfProperty } from '../property/model/property.model';
 
 const useStyles = makeStyles((theme) => ({
     exportContainer: {
@@ -24,20 +27,34 @@ const useStyles = makeStyles((theme) => ({
         alignItems: 'center',
         marginBottom: theme.spacing(2),
     },
+    title: {
+        marginTop: theme.spacing(5),
+    },
     table: {
         minWidth: 650,
     },
     bold: {
         fontWeight: 'bold',
     },
+    content: {},
+    propertyDetails: {
+        maxWidth: '40%',
+        marginBottom: theme.spacing(2),
+        marginTop: theme.spacing(2),
+    },
 }));
 
 const RentScheduleTable = () => {
+    const componentRef = useRef(null);
+    const handlePrint = useReactToPrint({
+        content: () => componentRef.current,
+    });
+
     const classes = useStyles();
 
     const { id } = useParams<{ id: string }>();
 
-    const property = useAppSelector(selectProperties).filter((property) => property.id === id);
+    const property = useAppSelector(selectProperties).filter((property) => property.id === id)[0];
     const rentalUnits = useAppSelector(selectRentalUnits).filter((unit) => unit.propertyId === id);
     const tenancies = useAppSelector(selectTenancies).filter(
         (tenant) => tenant.rentalUnitId === rentalUnits[0].id
@@ -55,54 +72,85 @@ const RentScheduleTable = () => {
         <>
             <div className={classes.exportContainer}>
                 <Typography variant={'h6'}>Export as PDF</Typography>
-                <IconButton>
+                <IconButton onClick={handlePrint}>
                     <GetAppOutlinedIcon />
                 </IconButton>
             </div>
-            <TableContainer component={Paper}>
-                <Table className={classes.table} aria-label="rent-schedule table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Ewid ID</TableCell>
-                            <TableCell align="right">Rental Unit</TableCell>
-                            <TableCell align="right">Tenant</TableCell>
-                            <TableCell align="right">Area</TableCell>
-                            <TableCell align="right">Since</TableCell>
-                            <TableCell align="right">Until</TableCell>
-                            <TableCell align="right">Rent Amount</TableCell>
-                        </TableRow>
-                    </TableHead>
+            <div className={classes.content} ref={componentRef}>
+                <Typography className={classes.title} variant={'h5'}>
+                    Rent Schedule Report for {getDisplayNameOfProperty(property)}
+                </Typography>
+                <TableContainer className={classes.propertyDetails} component={Paper}>
                     <TableBody>
-                        {rentalUnits.map((unit) => {
-                            const tenant = getTenant(unit);
-                            return (
-                                <TableRow key={unit.ewid}>
-                                    <TableCell component="th" scope="row">
-                                        {unit.ewid}
-                                    </TableCell>
-                                    <TableCell align="right">{getDisplayNameOfRentalUnit(unit)}</TableCell>
-                                    <TableCell align="right">{tenant.id}</TableCell>
-                                    <TableCell align="right">{unit.surfaceInM2}</TableCell>
-                                    <TableCell align="right">{tenant.beginOfContract}</TableCell>
-                                    <TableCell align="right">{tenant.endOfContract}</TableCell>
-                                    <TableCell align="right">{tenant.rentNet}</TableCell>
-                                </TableRow>
-                            );
-                        })}
                         <TableRow>
-                            <TableCell className={classes.bold}>Total Liegenschaft</TableCell>
-                            <TableCell />
-                            <TableCell />
-                            <TableCell />
-                            <TableCell />
-                            <TableCell />
-                            <TableCell className={classes.bold} align={'right'}>
-                                {rentSum}
-                            </TableCell>
+                            <TableCell>Report created on</TableCell>
+                            <TableCell>{format(new Date(), 'do MMMM Y')}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Property ID</TableCell>
+                            <TableCell>{property.egid}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Property</TableCell>
+                            <TableCell>{getDisplayNameOfProperty(property)}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Address</TableCell>
+                            <TableCell>{`${property.address.addressLine1}, ${property.address.postCode} ${property.address.city}`}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Owner</TableCell>
+                            <TableCell>{property.owner}</TableCell>
                         </TableRow>
                     </TableBody>
-                </Table>
-            </TableContainer>
+                </TableContainer>
+                <TableContainer component={Paper}>
+                    <Table className={classes.table} aria-label="rent-schedule table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Ewid ID</TableCell>
+                                <TableCell align="right">Rental Unit</TableCell>
+                                <TableCell align="right">Tenant</TableCell>
+                                <TableCell align="right">Area</TableCell>
+                                <TableCell align="right">Since</TableCell>
+                                <TableCell align="right">Until</TableCell>
+                                <TableCell align="right">Rent Amount</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rentalUnits.map((unit) => {
+                                const tenant = getTenant(unit);
+                                return (
+                                    <TableRow key={unit.ewid}>
+                                        <TableCell component="th" scope="row">
+                                            {unit.ewid}
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            {getDisplayNameOfRentalUnit(unit)}
+                                        </TableCell>
+                                        <TableCell align="right">{tenant.id}</TableCell>
+                                        <TableCell align="right">{unit.surfaceInM2}</TableCell>
+                                        <TableCell align="right">{tenant.beginOfContract}</TableCell>
+                                        <TableCell align="right">{tenant.endOfContract}</TableCell>
+                                        <TableCell align="right">{tenant.rentNet}</TableCell>
+                                    </TableRow>
+                                );
+                            })}
+                            <TableRow>
+                                <TableCell className={classes.bold}>Total Liegenschaft</TableCell>
+                                <TableCell />
+                                <TableCell />
+                                <TableCell />
+                                <TableCell />
+                                <TableCell />
+                                <TableCell className={classes.bold} align={'right'}>
+                                    {rentSum}
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
         </>
     );
 };
