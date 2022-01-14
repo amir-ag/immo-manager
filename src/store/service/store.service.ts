fb28d9c491d986b29531ff491f52e7df01b67eef
Manuel Fischer <manuel.fischer@outlook.de>
2022-01-14 22:57:21 +0100

    Removed "debugger" statement

--- src/store/service/store.service.ts
+++ src/store/service/store.service.ts
@@ -34,7 +34,6 @@ export const triggerNotificatorError = async (
     messagePrefix: string,
     error?: any
 ) => {
-    debugger;
     let errorMessage = '';
 
     if (error && typeof error === 'string') {
